7e287301503988fc368bc0c45970f24368684ba1
miguelco2000 <miguelco2000@gmail.com>
2022-02-14 18:55:56 +0100

    Change in basic_template

--- R/json_templates.R
+++ R/json_templates.R
@@ -4,7 +4,7 @@ basic_template <- paste0('{
 "R": {
         "type": "dir", "name": "R",
         "global_R": {"type": "file", "name": "global.R",
-		             "content": "DIR_DATA <- here::here(\'data\')\\n\\nDIR_DL_DATA <- file.path(DIR_DATA, \'datalake\')\\nDIR_DW_DATA <- file.path(DIR_DATA, \'datawarehouse\')\\n\\nDIR_OUTPUTS <- here::here(\'outputs\')\\n\\nDIR_OUTPUTS_REPORTS <- file.path(DIR_OUTPUTS, \'reports\')\\nDIR_OUTPUTS_FILES <- file.path(DIR_OUTPUTS, \'files\')"
+		             "content": "DIR_DATA <- here::here(\'data\')\\n\\nDIR_RAW_DATA <- file.path(DIR_DATA, \'raw\')\\nDIR_CLEAN_DATA <- file.path(DIR_DATA, \'clean\')\\n\\nDIR_OUTPUTS <- here::here(\'outputs\')\\n\\nDIR_OUTPUTS_REPORTS <- file.path(DIR_OUTPUTS, \'reports\')\\nDIR_OUTPUTS_FILES <- file.path(DIR_OUTPUTS, \'files\')"
 								 },
         "src": {"type": "dir", "name": "src"},
         "scripts": {"type": "dir", "name": "scripts"}
